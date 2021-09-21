9aa4ac7a9dc4d2381553d3fcfbabf64b24a673ad
HS-hyeokjin <youngurwls@gmail.com>
2021-09-21 23:38:37 +0900

    Eigteen

--- 4.Eighteen/src/eighteen/Player.java
+++ 4.Eighteen/src/eighteen/Player.java
@@ -5,41 +5,39 @@ import java.util.Random;
 
 
 public class Player {
-	Scanner scanner = new Scanner(System.in);
+	static Scanner scanner = new Scanner(System.in);
 	
 	public String name;
-	public static int card[] = {0,0,0};
+	public int card[] = {0,0,0};
 
 	public Player(String name) {
 		this.name = name;
 	}
 	
-	public static int Turnone() {                              //첫번째 턴 메소드
-		Random randomValue = new Random();		
+	public int Turnone() {                             //첫번째 턴 메소드
+		
 		return card[0] = (int) ((Math.random()*8)+1);
 		
+	}
+	public int Turntwo() {                              //두번째 턴 메소드		
 		
+		return card[1] = (int) ((Math.random()*8)+1);
 		
 	}
-	public static int Turntwo() {                              //두번째 턴 메소드
-		Random randomValue = new Random();		
+	public int Turnthree() {                             //세번째 턴 메소드	
 		
-		return card[1] = ((randomValue.nextInt(8) + 1));
-		
+		return card[2] = (int) ((Math.random()*8)+1);
 		
 	}
-	public static int Turnthree() {                              //세번째 턴 메소드
-		Random randomValue = new Random();		
+	public void ShowCard(){		//카드 출력 메소드
 		
-		return card[2] = ((randomValue.nextInt(8) + 1));
-		
-		
-	}
-	public static void ShowCard(){
+		System.out.print(name + "     ");
+
 		for (int i = 0; i < card.length; i++) {
 			System.out.print(card[i] + "   ");
-
+		}
+		System.out.println();
 	}
-	}
+	
 }
 
